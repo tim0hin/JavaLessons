0d07a33951f88f9b99d7eb73f78156914e62231d
yury_timohin <yury_timohin@mail.ru>
2023-04-18 22:48:21 +0300

    Домашнее задание. Выполнил задачу №1

---
+++ sem1_task1.java
@@ -0,0 +1,14 @@
+public class sem1_task1 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(sumNumbers(n));
+    }
+
+    static int sumNumbers(int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+}
