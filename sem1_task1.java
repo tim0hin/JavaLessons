6a9670699e132bc96fb6c357aef6e1f8ace6a839
yury_timohin <yury_timohin@mail.ru>
2023-04-18 22:56:17 +0300

    Добавлено условие в задачу №1

--- sem1_task1.java
+++ sem1_task1.java
@@ -1,5 +1,6 @@
 public class sem1_task1 {
     public static void main(String[] args) {
+        // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
         int n = 3;
         System.out.println(sumNumbers(n));
     }
