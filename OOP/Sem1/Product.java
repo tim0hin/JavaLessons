3fe67a61e1f536ccc3a91eda288b5da81abe405a
yury_timohin <yury_timohin@mail.ru>
2023-05-27 13:30:49 +0300

    Задачи с семинара №1 по ООП.

---
+++ OOP/Sem1/Product.java
@@ -0,0 +1,18 @@
+package OOP.Sem1;
+
+/**
+ * Класс "Товар", содержащий данные о товаре
+ */
+public abstract class Product {
+    protected String name;
+    protected int price;
+
+    // геттеры
+    public abstract String getName();
+    public abstract int getPrice();
+
+    // сеттеры
+    public abstract void setName(String name);
+    public abstract void setPrice(int price);
+   
+}
---
+++ OOP/Sem1/WaterVendingMachine.java
@@ -0,0 +1,19 @@
+package OOP.Sem1;
+
+import java.util.LinkedList;
+
+/**
+ * WaterVendingMachine
+ */
+public class WaterVendingMachine implements VendingMachine{
+    private LinkedList<Product> waters;
+
+    public void putProduct(LinkedList<Product> item){
+        this.waters = item;
+    }
+
+    public Product getProduct(){
+        return waters.pollLast();
+    }
+    
+}
